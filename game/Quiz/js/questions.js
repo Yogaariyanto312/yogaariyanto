0d48f67fdd9a5fa521c6f0bf993b7f3ae6f668dd
yogaariyanto <yogasisadboydarimanyar87.gmail.comgit config --global user.name yoga>
2022-03-25 15:15:58 +0700

    first

---
+++ game/Quiz/js/questions.js
@@ -0,0 +1,70 @@
+// membuat array dan melewati nomor, pertanyaan, opsi, dan jawaban
+let questions = [
+    {
+    numb: 1,
+    question: "Burung terbang dengan?",
+    answer: "Semaunya",
+    options: [
+      "Seniatnya",
+      "Semaunya",
+      "Seenaknya",
+      "Sayap"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Bandung lautan?",
+    answer: "Air",
+    options: [
+      "Api",
+      "Air",
+      "Laut",
+      "Sungai"
+    ]
+  },
+    {
+    numb: 3,
+    question: "kalau AC hidup apa reaksinya?",
+    answer: "Hidup",
+    options: [
+      "panas",
+      "Hidup",
+      "Dingin",
+      "anget"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Anda ikut berlomba. Anda menyalip orang di posisi nomor dua. Dan sekarang anda di posisi nomor berapa?",
+    answer: "2",
+    options: [
+      "1",
+      "2",
+      "3",
+      "Semua jawaban salah"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  
+     {
+     numb: 6,
+     question: "Your Question is Here",
+     answer: "Correct answer of the question is here",
+     options: [
+       "Option 1",
+       "option 2",
+       "option 3",
+       "option 4"
+     ]
+   },
+];
